098893c4a492c9bcd783176c8dec0792844edd07
Abhijith Nair <abhijithnair884@gmail.com>
2022-10-13 22:01:30 +0530

    added update operation

--- Django_app/employee_register/urls.py
+++ Django_app/employee_register/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
 
-    path("", views.employee_form),
-    path("list/", views.employee_List)
+    path("", views.employee_form,name='employee_insert'),
+    path("<int:id>/",views.employee_form,name='employee_update'),
+    path("list/", views.employee_List,name='employee_list')
 
 ]
