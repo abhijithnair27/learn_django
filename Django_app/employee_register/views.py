210869ec09ba7f87ec135a5764e378b006d96aef
Abhijith Nair <abhijithnair884@gmail.com>
2022-10-13 21:30:35 +0530

    added read operation

--- Django_app/employee_register/views.py
+++ Django_app/employee_register/views.py
@@ -1,11 +1,13 @@
 from pickle import GET
 from django.shortcuts import render,redirect
 from .forms import EmployeeForm
+from .models import Employee
 
 # Create your views here.
 
 def employee_List(request):
-    return render(request, "employee_register/employee_list.html")
+    context = {'employee_list':Employee.objects.all()}
+    return render(request, "employee_register/employee_list.html",context)
 
 
 def employee_form(request):
