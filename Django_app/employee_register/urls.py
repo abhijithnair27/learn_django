475a7c6ead5e7bbbf1ae95ff215d564ce03a72c7
Abhijith Nair <abhijithnair884@gmail.com>
2022-10-13 22:10:39 +0530

    added delete operation

--- Django_app/employee_register/urls.py
+++ Django_app/employee_register/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     path("", views.employee_form,name='employee_insert'),
     path("<int:id>/",views.employee_form,name='employee_update'),
+    path("delete/<int:id>/",views.employee_delete,name='employee_delete'),
     path("list/", views.employee_List,name='employee_list')
 
 ]
