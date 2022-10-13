186e09f171f7e261096506134d210ed7ddce5723
Abhijith Nair <abhijithnair884@gmail.com>
2022-10-13 17:06:00 +0530

    Added Models to Employee_register

--- Django_app/Django_app/settings.py
+++ Django_app/Django_app/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = "Django_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "EmployeeDB",
+        "USER": "postgres",
+        "PASSWORD": "Qwerty@27",
+        "HOST": "localhost"
     }
 }
 
